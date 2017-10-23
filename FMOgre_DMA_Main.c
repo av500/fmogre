f88a527faf750cff4d34ff7713c66bd3baec8031
Vladimir Pantelic <vladimir@pantelic.net>
2017-10-23 12:28:06 +0200

    changes by Timo Rozendal, 09.16.2016
    Signed-off-by: Vladimir Pantelic <vladimir@pantelic.net>

--- FMOgre_DMA_Main.c
+++ FMOgre_DMA_Main.c
@@ -15,6 +15,18 @@
  *    use without other license)
  */
 
+
+
+/* edited by Timo Rozendal, all these edits are mentioned with TIMO EDIT in the comments
+ * Reasons for editing: 
+ * - sync was connected to another port on the latest design (RA9  in stead of RB9)
+ * - rerouted v/oct input to an7 to make use of the trimmer in the latest design
+ * - enabling external voltage reference (requires new edits in circuit)
+ * - using an8 for pitch knob input (was needed since an0 is used for voltage reference)
+ * 
+ * these edits still need to be incorporated in the other comments
+ */
+
 /*
  *    Steps in the process:
  *
@@ -167,8 +179,10 @@ volatile unsigned long curbasephase;
 volatile long curpitchval;
 volatile long curpitchincr;
 volatile long curfreqmod;
+volatile long cvpm_dv, old_cvpm, older_cvpm, cvpm_predicted, cvpm_errpredmult;
 volatile long curphasemod;
 volatile long curaltphasemod;
+volatile long curresolution;
 volatile long curfbgain;
 //     AD converter stuff
 int which_adc;
@@ -192,14 +206,33 @@ static unsigned int dma_eng_addr;
 volatile unsigned int cvdata[16] __attribute__ ((space(dma),aligned(256)));
 volatile unsigned adc_data;
 volatile unsigned adc_chan;
-#define cvpitch (cvdata[0])
+
+//#define REVISION1
+
+#ifdef REVISION1
+#define cvpitch (cvdata[7]) //was 0 TIMO EDIT
+#define cvfb (cvdata[1])
+#define cvfm (cvdata[2])
+#define cvpm (cvdata[3])
+#define cvpmknob ((long)4095 - cvdata[4])
+#define cvfmknob ((long)4095 - cvdata[5])
+#define cvfbknob ((long)4095 - cvdata[6])
+#define cvpitchknob ((long)4095 - cvdata[0])   //was  7 TIMO EDIT
+#else
+
+#define cvpitch (cvdata[7]) //was 0 TIMO EDIT
 #define cvfb (cvdata[1])
 #define cvfm (cvdata[2])
 #define cvpm (cvdata[3])
 #define cvpmknob (cvdata[4])
 #define cvfmknob (cvdata[5])
 #define cvfbknob (cvdata[6])
-#define cvpitchknob (cvdata[7])
+#define cvpitchknob (cvdata[8])   //was 0 and before that 7 TIMO EDIT
+
+#endif
+
+
+
 #define ADC_FIRST (0)
 #define ADC_LAST (7)
 #define PBUF_LEN 4096
@@ -214,12 +247,12 @@ volatile unsigned adc_chan;
 __attribute__ ((far)) unsigned int pbuf [PBUF_LEN];   
 volatile unsigned pbindex = 0;
 
-static short adc_inverted [8] = {1,1,1,1,0,0,0,0};
+//static short adc_inverted [8] = {1,1,1,1,0,0,0,0};
 //static short adc_sequence [16] = {0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7};
 
 //   Pins RB5, RB6, and RB7 are the LED port.
 //   Pin  RB8 is SYNC OUT, pin RB9 is SYNC IN.
-#define SYNC_IN (PORTBbits.RB9)
+#define SYNC_IN (PORTAbits.RA9) ///TIMO edit  RB9->RA9
 //     The wavetable for sine waves are in wavetable.h
 #include "wavetable.h"
 
@@ -275,7 +308,7 @@ void __attribute__((__interrupt__,__auto_psv__)) _ADC1Interrupt(void)
     // TESTPOINT1 = 0;   // telltale
 
 }
- #endif
+#endif
 
 
 void __attribute__((__interrupt__,__auto_psv__)) _DAC1LInterrupt(void)
@@ -302,7 +335,7 @@ void __attribute__((__interrupt__,__auto_psv__)) _DAC1RInterrupt(void)
         //   Note we store this in 12 bit (0-4095) mode; when we do our
         //   windowing multiply we get it out to 16 bits
         pbuf[pbindex] = (((unsigned long) 4095) - cvfb) ;
-        if (PORTBbits.RB9 == 0) {pbindex++; };   // is SYNC / FREEZE on or off?
+        if (PORTAbits.RA9 == 0) {pbindex++; };   // is SYNC / FREEZE on or off? ///TIMO edit  RB9->RA9
         if (pbindex >= PBUF_LEN) pbindex = 0;
     }
 
@@ -323,7 +356,7 @@ void __attribute__((__interrupt__,__auto_psv__)) _DAC1RInterrupt(void)
     //   Add in linear frequency to the pitch base phase.
 //#define INTERRUPT_CURFREQMOD
 #ifdef INTERRUPT_CURFREQMOD
-    curfreqmod = cvfmknob * (cvfm- 2048) << 5;
+    curfreqmod = cvfmknob * (((long)2047 - cvfm) << 6;
 #endif
     curbasephase = curbasephase + curfreqmod;
 
@@ -390,8 +423,51 @@ void __attribute__((__interrupt__,__auto_psv__)) _DAC1RInterrupt(void)
     //    Calculate the phase modulation.   Can do here or baseline.
 #define INTERRUPT_CURPHASEMOD
 #ifdef INTERRUPT_CURPHASEMOD
-    curphasemod = ((((long)2047 - cvpm) * cvpmknob) >> 10);
-    curaltphasemod = (( (long)2047 - cvpm) * cvpmknob) + 256;
+#define DEADBAND 0
+#ifdef PRED_MULT
+    if (cvpm != old_cvpm) 
+    {
+        if (cvpm_predicted < cvpm + DEADBAND ) 
+            cvpm_errpredmult = cvpm_errpredmult + ((cvpm - old_cvpm) > 0 ? +1 : -1 );
+        if (cvpm_predicted > cvpm - DEADBAND ) 
+            cvpm_errpredmult = cvpm_errpredmult + ((cvpm - old_cvpm) > 0 ? -1 : +1 );
+        //cvpm_errpredmult = 2048 - cvfmknob;
+        cvpm_dv = ((((long)cvpm) - ((long)old_cvpm)) * cvpm_errpredmult ) / 256;
+        older_cvpm = old_cvpm;
+        old_cvpm = (long)cvpm;
+        cvpm_predicted = (long)cvpm;
+    }
+    else
+    {
+        cvpm_predicted = cvpm_predicted + cvpm_dv ;
+    }
+#endif
+
+    //   New method- keep current, old, and older.   
+    if (cvpm != old_cvpm)
+    {
+        older_cvpm = old_cvpm;
+        cvpm_predicted = older_cvpm;
+        old_cvpm = cvpm;
+    }    
+    else
+    { 
+        //cvpm_predicted = (cvpm_predicted + cvpm_predicted + cvpm_predicted + old_cvpm) >> 2;
+        cvpm_predicted = (cvpm_predicted + old_cvpm) >> 1;   // use this for 26.4 KHz max freq 
+        //cvpm_predicted = (cvpm_predicted + old_cvpm + old_cvpm + old_cvpm) >> 2;
+    }
+    
+    curphasemod = (( (long)2047 - cvpm_predicted) * cvpmknob) >> 10;
+    //curphasemod = ((((long)2047 - cvpm) * cvpmknob) >> 10);
+    //curphasemod = (( (long)2047 - cvpm) * cvpmknob) >> 12;
+    //    curphasemod = (( (long)2047 - cvpm) * cvpmknob) + 256;
+    //    curphasemod =  (curphasemod + (curphasemod << 3) 
+    //            + ((((long)2047 - cvpm) * cvpmknob))) >> 12;
+    //curresolution = (cvpmknob) ;  //  goes from lo (0) to 4096 (hi)
+    //curresolution = ((long)4095 - cvpm);    // this expr goes from 0 at -5V to 4095 at +5V
+    //curresolution = ((cvpmknob) * ((long)4095 - cvpm)) >> 12;  // works jack goes +-5 
+    curresolution = ((cvpmknob) * ((long)4095 - cvpm)) >> 12;  // works jack goes +-5 
+
 #endif
     final_phase_fm_fb_pm = 0x00000FFF &
                     (
@@ -422,22 +498,48 @@ void __attribute__((__interrupt__,__auto_psv__)) _DAC1RInterrupt(void)
     dist2 = 16 - dist1;
     dac1la = SAMPLESWITCH ?  
             pbuf [final_phase_fm_fb_pm] * dist1  // was enabled
-            //+ 2048 * (16 - dist1)
             + pbuf [fpffpp] * dist2  //  was enabled 
-            // pbuf [final_phase_fm_fb_pm] * 16    
-            : sine_table [0X00000fff & (final_phase_fm_fb_pm + 2048)];
-    if (RESOLUTIONSWITCH)
+            : sine_table [0X00000fff & (final_phase_fm_fb_pm)];
+    //if (RESOLUTIONSWITCH)
     {
-    
+        //   DANGER DANGER DANGER!!!  Use muldiv decimation only with DAC dividers 
+        //   of 6 or greater!   Otherwise, the interrupt cannot keep up with the
+        //   demand of the DAC oversampling hardware and you'll underrun which makes
+        //   lots of negative-going pulses.  Not A Good Thing!
+#define MULDIV_DECIMATION
+#ifdef MULDIV_DECIMATION
         //   Next step:  decimation / resolution reduction - uses integer divide
         //   then multiply by same amount to reduce the resolution.
-        dac1lb = (((dac1la - 32768) / ((curaltphasemod)>>8))
+        dac1lb = (((dac1la - 32768) / ((curaltphasemod)>> 8))
                 * ((curaltphasemod)>>8))
                 + 32768;
+#endif
+//#define SHIFT_DECIMATION
+#ifdef SHIFT_DECIMATION
+        dac1lb = (((dac1la - 32768) >> (curaltphasemod >> 8)) 
+                << (curaltphasemod >> 8)) + 32768;
+                
+#endif        
+//#define TABLE_MASK_DECIMATION
+#ifdef TABLE_MASK_DECIMATION
+        //    A work in progress.   Not really satisfactory yet.
+        dac1lb = dac1la & highorderbittable [ ((4095 - cvpm) * cvpmknob) >> 14];
+#endif
+//#define CLIP_DECIMATION
+#ifdef CLIP_DECIMATION
+        //   Cheaper, faster decimation (actually, clipping)!   No divide!
+        //dac1lb = dac1la & graytable [curresolution];
+        //dac1lb = dac1la > 65535 - (curresolution << 3) ? 65535 :
+        //        ( dac1la <  (curresolution << 3) ? 0 :
+        //           dac1la );
+#endif
     }
-    DAC1LDAT = RESOLUTIONSWITCH ? dac1lb : dac1la;
+    DAC1LDAT =  RESOLUTIONSWITCH ? dac1lb : dac1la;
+   
     
-    //   and hard sync out - tried doing this in base loop, too much jitter.
+    //   Hard sync out - tried doing this in base loop, too much jitter.  
+    //    Even here, there's a lag of about 0.2 millisecond in the DAC path
+    //    due to the 256x oversampling versus the direct path here for SYNC OUT
     //    Note that Hard Sync IN changes if we're in granular/sampling mode to
     //    become _FREEZE_INPUT_SAMPLES_
     if (SAMPLESWITCH)
@@ -503,11 +605,15 @@ int main(int argc, char** argv) {
     TRISAbits.TRISA1 = 1;
     TRISAbits.TRISA4 = 1;   //  RA4 is switch 3(sine versus sample)
     TRISAbits.TRISA8 = 1;   //  RA8 is switch 1 (VCO versus LFO)
+    TRISAbits.TRISA9 = 1;  //   Pin RA9 is SYNC IN. TIMO EDIT
+
 
     TRISC = 0x0;    //   Inputs on RC0-1 (ADC 6 and 7)
     TRISCbits.TRISC0 = 1;
     TRISCbits.TRISC1 = 1;
-
+    #ifndef REVISION1
+    TRISCbits.TRISC2 = 1;//TIMO EDIT (to use an8 in stead of an0)
+    #endif
     TRISB = 0x0;    //   Inputs on RB0-3 (ADC 2,3,4,5), RB8-9 (switches)RB12-15 (DACs)
     TRISBbits.TRISB0 = 1;
     TRISBbits.TRISB1 = 1;
@@ -522,7 +628,7 @@ int main(int argc, char** argv) {
     TRISBbits.TRISB7 = 0;  //  RB7 is negative phase
     PORTBbits.RB7 = 1;
     TRISBbits.TRISB8 = 0;  //   Pin RB8 is SYNC OUT,
-    TRISBbits.TRISB9 = 1;  //   Pin RB9 is SYNC IN.
+    //TRISBbits.TRISB9 = 1;  //   Pin RB9 is not SYNC IN. TIMO EDIT
     TRISBbits.TRISB12 = 1;
     TRISBbits.TRISB13 = 1;
     TRISBbits.TRISB14 = 1;
@@ -537,7 +643,22 @@ int main(int argc, char** argv) {
     DAC1CONbits.DACEN = 1;    // enable the audio dac
     DAC1CONbits.AMPON = 1;    // enable the output amplifier
     DAC1CONbits.FORM = 0;     //  unsigned data (0 = unsigned data)
-    DAC1CONbits.DACFDIV = 8;  // (was 3, then 6) divide Fosc to drive 
+    //
+    //    CHANGE THE DACFDIV TO CHANGE OUTPUT SAMPLING SPEED!!!
+    //     The following pitch-DACFDIV values are relative, of course.
+    //    (Note, this is not sample RATE.  This is sine wave rendered)
+    //    8 = 14.7 KHz max pitch
+    //    7 = 16.5 KHz max pitch
+    //    6 = 18.9 KHz max pitch, 50 KHz phase update, fastest for MULDIV 
+    //    5 = 22.0 KHz max pitch, 50 KHz phase update
+    //    4 = 26.4 KHz max pitch, 50 KHz phase update
+    //    3 = 33.0 KHz max pitch, 33 KHz phase update
+    //    2 = 44.07 kHz max pitch, 28 KHz phase update 
+    //    1 .... antialias malfunction.  Don't use this.
+    //    To be honest, it sounds really good at either 3 or 4 to me if you want
+    //    dogs to hear it, and just fine at 6, which allows MULDIV-decimation.
+    
+    DAC1CONbits.DACFDIV = 6;  // (was 3, then 6, then 8) divide Fosc to drive 
                               //  interpolator. 3 = 83.333KHz @40MIPS
     DAC1STAT = 0xFFFF;        //  everything on
     DAC1STATbits.LITYPE = 1;  // 0 means interrupt on Left LIFO not full
@@ -581,7 +702,11 @@ int main(int argc, char** argv) {
     AD1CON1bits.DONE = 0;      //  set ADC to not be done yet (HW sets to 1 on completion)
 
     AD1CON2bits.CSCNA = 1;     //  1=scan inputs, 0=do not scan inputs
-    AD1CON2bits.VCFG = 0 ;     //  nothing fancy, swing is Vss to Vdd
+    #ifdef REVISION1
+    AD1CON2bits.VCFG = 0 ;     //  nothing fancy, swing is Vss to Vdd   //TIMO EDIT, was 0, changed it to 1 for external positive ref
+    #else
+    AD1CON2bits.VCFG = 1 ;
+    #endif
     AD1CON2bits.CHPS = 0;      //  use just 1 ADC channel
     AD1CON2bits.SMPI = 7;      //  (was 7)increment DMA adddress after N+1 converts
     AD1CON2bits.BUFM = 0;      //  always start buffer fill at 0
@@ -598,8 +723,13 @@ int main(int argc, char** argv) {
     AD1CHS0bits.CH0SB = 0;     //  channel B pos input is input AD0
     AD1CHS0bits.CH0NA = 0;     //  channel A negative input is Vref-
     AD1CHS0bits.CH0SA = 0;     //  channel A positive input is input AD0
+#ifdef REVISION1
     AD1CSSL = 0x00FF;            //  AN0-7 enabled for scan
     AD1PCFGL = 0xFF00;         //  enable ADC on low 8 AD pins.
+#else
+    AD1CSSL = 0x01FE;            //  AN0-7 enabled for scan  //TIMO EDIT was 00FF (to use an8 in stead of an0)
+    AD1PCFGL = 0xFE00;         //  enable ADC on low 8 AD pins. //TIMO EDIT was FF00 (to use an8 in stead of an0)
+#endif
     IFS0bits.AD1IF = 0;        //  Clear the AD interrupt flag
     IEC0bits.AD1IE = 0;        //  Do not enable AD interrupt 
     AD1CON1bits.ADON = 1;      // turn on the ADC
@@ -607,10 +737,19 @@ int main(int argc, char** argv) {
     //   Set up the TIMER3 to keep kicking the ADC.   We use TIMER3 because
     //   only timer3 has a direct wire to kick the ADC into sample/convert.
     TMR3 = 0x0000;
-    PR3 = 4999;                // 4999 = prescale to 125 uSec (8 KHz)
-    PR3 = 250;                 // 250 = prescale to 160 KHz
-    PR3 = 200;                  //  200 = prescale to 200 KHz
-    //PR3 = 100;                 // 100 = prescale to 400 KHz
+    //              PR3 is the scale factor for the ADC converter.  400 KHz
+    //  sounds pretty good (that's update at 50 KHz per input), PR3=100.
+    //   Yes, there is crosstalk from one channel to another.  But it's 
+    //   not improved by slowing down the ADC (yes, I've tested this all the way
+    //   down to 8 KHz sample rate and going slower doesn't help; even more
+    //   amazingly it doesn't sound that bad at all even though the scope trace
+    //   is utter crud.)
+    //PR3 = 4999;                // 4999 = prescale to 125 uSec (8 KHz)
+    //PR3 = 250;                 // 250 = prescale to 160 KHz
+    //PR3 = 200;                  //  200 = prescale to 200 KHz
+    //PR3 = 150;
+    PR3 = 100;                 // 100 = prescale to 400 KHz
+    //PR3 = 80;                  //  80 = prescale to 500 KHz  (maximum rated)
     T3CONbits.TSIDL = 0;       // keep timing in idle
     IFS0bits.T3IF = 0;         // Clear T3 interrupt
     IEC0bits.T3IE = 0;         // Disable T3 interrupt
@@ -628,6 +767,7 @@ int main(int argc, char** argv) {
 
     //   Zero the buffer index for starters.
     pbindex = 0;
+    cvpm_errpredmult = 0;
 
 /*   Not used any more because we do DMA now   
     AD1CON1bits.ADON = 1;      //  turn on the AD converter itself.
@@ -692,7 +832,7 @@ int main(int argc, char** argv) {
 
 #define BASELINE_CVPITCHINCR
 #ifdef BASELINE_CVPITCHINCR
-    curpitchval = cvpitchknob + (2047 - cvpitch) ;
+    curpitchval = cvpitchknob + ((long)2047 - cvpitch) ;
     //   use exp_table to get exp freq resp, or if RA8 (aka FLOSWITCH) is turned off
     //   then we're in LFO mode and we use the value rightshifted 12 bits (very low
     //   frequency).
@@ -710,7 +850,8 @@ int main(int argc, char** argv) {
     //   We can calculate linear frequency modulation here.
 #define BASELINE_CURFREQMOD
 #ifdef BASELINE_CURFREQMOD
-    curfreqmod = cvfmknob * (((long)2047) - cvfm) << 5;
+    curfreqmod = cvfmknob * (((long)2047) - cvfm) << 6;
+    //curfreqmod = 0;
     //    Output RB6 high if we have negative frequency.  Note because
     //    of capacitance issues, we ALWAYS have RB6 on, but rather
     //    just turn on and off the tristate (TRISbits) for RB6
@@ -718,11 +859,14 @@ int main(int argc, char** argv) {
     TRISBbits.TRISB6 = curpitchincr + curfreqmod < 0 ? 0 : 1;
 #endif
 
-//#define BASELINE_CURPHASEMOD
+//#define BASELINE_CURPHASEMOD    
 #ifdef BASELINE_CURPHASEMOD
-    curaltphasemod = ((cvpm - 2048) * cvpmknob) + 256;
-    curphasemod = (((cvpm - 2048) * cvpmknob) >> 10);
+    curphasemod = ((((long)2047 - cvpm) * cvpmknob) >> 10);
+    curaltphasemod = (( (long)2047 - cvpm) * cvpmknob) + 256;
 #endif
+
+    curaltphasemod = (( (long)2047 - cvpm) * cvpmknob) + 256;
+
     //+ ((curphasemod ) / 2);
     //   Output RB7 driven high if we have negative phase.   Note that
     //   because of capacitance issues, we can't just output the bit; we
@@ -736,7 +880,7 @@ int main(int argc, char** argv) {
     //   Do we have a HARD SYNC IN request on pin RB9?
     //   GROT GROT GROT note that this hacky hysteresis could / should
     //   really be done in an interrupt.  Maybe later....
-    if (PORTBbits.RB9 == 1 && SAMPLESWITCH == 0 )
+    if (PORTAbits.RA9 == 1 && SAMPLESWITCH == 0 )  //TIMO EDIT RB9->RA9
     {
         if (oldhardsync == 0)
         {
